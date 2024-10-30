dcf7ac93756b6aef651532af8ae8681fea7a160c
Ahmed Samir <ahmedsamirsoftwareengineer@gmail.com>
2024-10-30 12:17:20 +0300

    nationalities&bloodType&religions

---
+++ app/Religion.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+
+class Religion extends Model
+{
+    use HasTranslations;
+
+    public $translatable = ['Name'];
+
+    protected $fillable = [
+        'Name',
+    ];
+}
