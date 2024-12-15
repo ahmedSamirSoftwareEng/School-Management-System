d611190a9c7ac37943f9cf25f45bc005419aeec4
Ahmed Samir <ahmedsamirsoftwareengineer@gmail.com>
2024-12-15 10:55:06 +0200

    attendence_report

--- app/Http/Controllers/Teachers/dashboard/StudentController.php
+++ app/Http/Controllers/Teachers/dashboard/StudentController.php
@@ -39,7 +39,7 @@ class StudentController extends Controller
                     $attendence_status = false;
                 }
 
-                Attendance::updateorCreate(['student_id' => $studentid], [
+                Attendance::updateorCreate(['student_id' => $studentid,   'attendence_date' => $attenddate], [
                     'student_id' => $studentid,
                     'grade_id' => $request->grade_id,
                     'classroom_id' => $request->classroom_id,
@@ -74,6 +74,43 @@ class StudentController extends Controller
             return redirect()->back();
         } catch (\Exception $e) {
             return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+        }
+    }
+
+    public function attendanceReport()
+    {
+
+        $ids = DB::table('teacher_section')->where('teacher_id', auth()->user()->id)->pluck('section_id');
+        $students = Student::whereIn('section_id', $ids)->get();
+        // return $students;
+        return view('pages.Teachers.students.attendance_report', compact('students'));
+    }
+    public function attendanceSearch(Request $request)
+    {
+        // return $request;
+        $request->validate([
+            'from'  => 'required|date|date_format:Y-m-d',
+            'to' => 'required|date|date_format:Y-m-d|after_or_equal:from'
+        ], [
+            'to.after_or_equal' => 'تاريخ النهاية لابد ان اكبر من تاريخ البداية او يساويه',
+            'from.date_format' => 'صيغة التاريخ يجب ان تكون yyyy-mm-dd',
+            'to.date_format' => 'صيغة التاريخ يجب ان تكون yyyy-mm-dd',
+        ]);
+
+
+        $ids = DB::table('teacher_section')->where('teacher_id', auth()->user()->id)->pluck('section_id');
+        $students = Student::whereIn('section_id', $ids)->get();
+
+        if ($request->student_id == 0) {
+
+            $Students = Attendance::whereBetween('attendence_date', [$request->from, $request->to])->get();
+            // return $Students;
+            return view('pages.Teachers.students.attendance_report', compact('Students', 'students'));
+        } else {
+
+            $Students = Attendance::whereBetween('attendence_date', [$request->from, $request->to])
+                ->where('student_id', $request->student_id)->get();
+            return view('pages.Teachers.students.attendance_report', compact('Students', 'students'));
         }
     }
 }
