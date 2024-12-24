bccd7fc856aeaaaf13567dfa98e6d719bd7e206e
Ahmed Samir <ahmedsamirsoftwareengineer@gmail.com>
2024-12-24 12:50:17 +0200

    Get Fees

--- app/My_Parent.php
+++ app/My_Parent.php
@@ -4,8 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class My_Parent extends Model
+
+class My_Parent extends Authenticatable
 {
     use HasTranslations;
     protected $table = 'my__parents';
